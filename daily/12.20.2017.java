afcfbd838b69efb5cff5cd0d4c4bd4c5cdabc32e
Runze Wang <rwang@mettel.net>
2017-12-20 18:40:28 -0500

    Add todays task list

--- daily/12.20.2017.java
+++ daily/12.20.2017.java
@@ -3,4 +3,4 @@
 // 2. Redo the reverse linkedlist by pairs
 // 3. Understand how to solve the N-queens using the 2 methods from Laicode
 // 4. Finished the Spiral array I
-// 5. Review the lecture of DPI
+// 5. Review the lecture of DPI -> Fibinacci sequence, cut rope(recursion, 左大段+右大段， 左大段+右小段), Longest ascending array, Jump game
