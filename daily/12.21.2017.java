da888fa60bcfe843779d21a42d8853a3f60c641a
Runze Wang <rwang@mettel.net>
2017-12-21 18:18:08 -0500

    12.21.2017

--- daily/12.21.2017.java
+++ daily/12.21.2017.java
@@ -5,3 +5,59 @@
 // 4. Finish the Spiral array I
 // 5. Finish cut rope(recursion, 左大段+右大段， 左大段+右小段), Longest ascending array, Jump game
 // 6. Work on the dicitionary problem and shortest dp
+
+// Reserve String using recursion
+// Time complexity: O(n)
+// Space complexity: O(2n) = O(n)
+public class Solution {
+  // Using recursion to reverse the string
+  public String reverse(String input) {
+    // Write your solution here.
+    if (input == null || input.length() <= 1) {
+      return input;
+    }
+    char[] array = input.toCharArray();
+    this.helper(array, 0, array.length-1);
+    return new String(array);
+  }
+
+  private void helper(char[] array, int start, int end) {
+    if (start >= end) {
+      return;
+    }
+    // Swap [start] and [end]
+    this.swap(array, start, end);
+    // Recursion for start++ and end--
+    this.helper(array, start+1, end-1);
+  }
+
+  private void swap(char[] array, int i, int j) {
+    char c = array[i];
+    array[i] = array[j];
+    array[j] = c;
+  }
+}
+
+// Spiral array
+public class Solution {
+  public List<Integer> spiral(int[][] matrix) {
+    // Write your solution here.
+    List<Integer> result = new LinkedList<>();
+    if (matrix == null) {
+      return result;
+    }
+    if (matrix.length <= 1) { // Need to double check if we can initiate a linkedList from matrix
+      result = new LinkedList(matrix);
+    }
+    // Check top, right, bottom, left
+    int left = 0;
+    int right = matrix.length-1;
+    while (left <= right) {
+      // Go through the top row
+      for (int i = left; ) {
+
+      }
+    }
+    return result;
+  }
+}
