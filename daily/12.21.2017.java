9622da97471c138dd24e4839b7fbdb99230b8389
Runze Wang <rwang@mettel.net>
2017-12-21 18:02:43 -0500

    12.21.2017

---
+++ daily/12.21.2017.java
@@ -0,0 +1,7 @@
+// Task:
+// 1. Using recursion to reverse the String
+// 2. Redo the reverse linkedlist by pairs
+// 3. Understand how to solve the N-queens using the 2 methods from Laicode
+// 4. Finish the Spiral array I
+// 5. Finish cut rope(recursion, 左大段+右大段， 左大段+右小段), Longest ascending array, Jump game
+// 6. Work on the dicitionary problem and shortest dp
