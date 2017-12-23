156a373a4c12d6bf57c9a3d95648f3e72591c15f
Runze Wang <runze@Runzes-MacBook-Air.local>
2017-12-23 11:23:36 -0500

    12.22.2017

---
+++ daily/12.22.2017.java
@@ -0,0 +1,6 @@
+// 1. Finished the spiral array 1,2,3
+// 2. Finished the Longest Ascending array
+// 3. Finished the Jump Game 1
+// 4. Finished the Cut rope
+// 5. Rewrited the N-Queens using Java Solution method
+// 6. Checked the recursion method of Reverse String made by 12.21.2017
