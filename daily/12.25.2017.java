b7c7840a5e29fbd2fcbf1d6845197ab5ff4e23a8
Runze Wang <rwang@mettel.net>
2017-12-25 12:14:39 -0500

    Add the README

--- daily/12.25.2017.java
+++ daily/12.25.2017.java
@@ -1,2 +1,4 @@
-// Try to finish the Udemy course of ReactJS
-// Finish the assignement of DP2 and Smapling
+// 1. Try to finish the Udemy course of ReactJS
+// 2. Finish the assignement of DP2 and Smapling
+// 3. Learn how to config the webpack from scratch
+// 4. Design the workflow of how to implement the `AddAdditionalLine` ticket
