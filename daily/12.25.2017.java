b1c868931ae4dc5dfe8bd91b9b3eba49f1105518
Runze Wang <runze@Runzes-MacBook-Air.local>
2017-12-28 01:30:54 -0500

    init 12.25.2017

--- daily/12.25.2017.java
+++ daily/12.25.2017.java
@@ -1,4 +1,4 @@
-// 1. Try to finish the Udemy course of ReactJS
-// 2. Finish the assignement of DP2 and Smapling
-// 3. Learn how to config the webpack from scratch
-// 4. Design the workflow of how to implement the `AddAdditionalLine` ticket
+// 1. (EXCEPT REDUX FORM) Try to finish the Udemy course of ReactJS
+// 2. (NOT START)Finish the assignement of DP2 and Smapling
+// 3. (Done) Learn how to config the webpack from scratch
+// 4. (Done) Design the workflow of how to implement the `AddAdditionalLine` ticket
